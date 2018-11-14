574fbb11a6df483c784472b3bbce4bcfda8ac9bb
Sophie Henry <sophiesophiehenry@googlemail.com>
2018-11-14 16:39:22 +0800

    updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+7.times do
+  restaurant = Restaurant.new(
+    name: Faker::Hipster.word,
+    phone_number: Faker::PhoneNumber.phone_number,
+    address: Faker::Address.street_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+  restaurant.save!
+end
